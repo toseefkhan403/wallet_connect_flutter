16c1722a5490d64a24707ad8e128dda995ab2681
toseefkhan403 <toseefkhan403@gmail.com>
2022-02-28 18:29:08 +0530

    add session request dialog & approve method (dart)

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -72,11 +71,13 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(onPressed: () {
-              showModalBottomSheet(context: context, builder: (c) => Container(child: Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                TextField(controller: uriController,),
-                ElevatedButton(onPressed: initConnection, child: Text('ok'))
-              ],),));
+              showDialog(context: context, builder: (c) => Dialog(
+                child: Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                  TextField(controller: uriController,),
+                  ElevatedButton(onPressed: initConnection, child: Text('ok'))
+                ],),
+              ));
             }, child: Text('Enter URI'),),
 
             ElevatedButton(onPressed: () {
@@ -94,15 +95,114 @@ class _MyHomePageState extends State<MyHomePage> {
     try {
       print("uriController.text: ${uriController.text}");
       var parameters = {'uri':uriController.text};
-      var value = await platform.invokeMethod('initConnection' , parameters );
+      var value = await platform.invokeMethod('initConnection' , parameters);
 
-      print("valueeee0 $value");
-      var json1 = json.decode(value);
-      print("valueeee1 ${json1}");
-      print("valueeee2 ${json1.toString()}");
+      print('value : $value');
+      showDialog(context: context, builder: (c) => initConnectionDialog(c,json.decode(value)));
 
+    } catch (e) {
+      print(e);
+    }
+  }
 
-      showDialog(context: context, builder: (c) => Dialog(child: Container(child: Text(value.toString()),),));
+  Widget initConnectionDialog(context, data) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          color: Color(0xff2B2F39),
+          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Connection Request",
+                      style: const TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600)),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: const Icon(Icons.close, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Image.network(data['icon'], height: 50, width: 50,),
+            Text(
+              " ${data['name']} \n ${data['description']} \n ${data['chains']} \n ${data['methods']}",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    // width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(0xff373C47)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Text('Reject',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ),
+                        onPressed: reject),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    // width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).buttonColor),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14.0),
+                        child: Text('Approve',
+                            style: Theme.of(context).textTheme.bodyText1),
+                      ),
+                      onPressed: approve,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void approve() async {
+    Navigator.pop(context);
+
+    try {
+      var value = await platform.invokeMethod('approve' , {'accountId' : '987338983394e08af4ec715e765373fe8c13302e'});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void reject() async {
+    Navigator.pop(context);
+
+    try {
+      var value = await platform.invokeMethod('reject');
     } catch (e) {
       print(e);
     }
